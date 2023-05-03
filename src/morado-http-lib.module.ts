d63a41297777a8b1ed1fac2a8a1e72f60880e568
Julian Mora <julian.mora@morado.app>
2023-05-03 12:32:43 -0500

    Remove ignore

--- src/morado-http-lib.module.ts
+++ src/morado-http-lib.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MoradoHttp } from './services/morado-http.service';
 import { AxiosService } from './services/axios.service';
-// @ts-ignore
+
 @Module({
   providers: [AxiosService, MoradoHttp],
   exports: [MoradoHttp],
